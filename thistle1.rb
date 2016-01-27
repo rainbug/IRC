c6abc9ed3f98a007de133bc5d0935a9cb16b51ec
rainbug <zanefry@gmail.com>
2016-01-27 01:21:36 -0600

    thistle1 should work

--- thistle1.rb
+++ thistle1.rb
@@ -1,4 +1,5 @@
-require "./IRCbot.rb"
+#!/usr/bin/ruby
+require "./IRCBot.rb"
 
 thistle = IRCBot.new "irc.rizon.net", "thistle"
 thistle.connect
@@ -7,9 +8,16 @@ thistle.join "#techbuds"
 Thread.new do
   loop do
     line = thistle.readline
+    puts line
+
+    if line.start_with? "PING"
+      thistle.send "PONG :I.am.thistle\r\n"
+    end
   
     if line.include? ":_thistle!~" and line.include? "PRIVMSG"
-      
+      thistle.message content(line)
     end
   end
 end
+
+loop {}
